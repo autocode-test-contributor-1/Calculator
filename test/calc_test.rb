2f23989c664755db964f2fd53dfac86f484c526b
Kirill Peshin <kirill_peshin@epam.com>
2021-01-18 17:11:38 +0300

    Calc

--- test/calc_test.rb
+++ test/calc_test.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require_relative '../src/calculator'
+require_relative '../lib/calculator'
 
 class CalcTest < Minitest::Test
   def setup
     @calc = Calculator.new('test')
   end
 
-  def test_addition
+  def test_addition_mini
     assert_equal 4, @calc.add(2, 2)
   end
 
-  def test_subtraction
+  def test_subtraction_mini
     assert_equal 2, @calc.subtract(4, 2)
   end
 
-  def test_division
+  def test_division_mini
     assert_equal 2, @calc.divide(4, 2)
   end
 
-  def multiply
+  def test_multiply_mini
     assert_equal 8, @calc.multiply(4, 2)
   end
 end
---
+++ test/super/supercalc_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../lib/super/super_calc'
+
+class SuperCalcTest < Minitest::Test
+  def setup
+    @super_calc = SuperCalculator.new('testSuper')
+  end
+
+  def test_addition_super
+    assert_equal 7, @super_calc.add_super(2, 2, 3)
+  end
+
+  def test_multiply_super
+    assert_equal 16, @super_calc.multiply_super(4, 2, 2)
+  end
+end
